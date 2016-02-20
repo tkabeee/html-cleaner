7cd4f89eb5ea84e91eb542340ac9926939f2cf90
tkabeee <tkabee@gmail.com>
2016-02-20 21:47:55 +0900

    react用定義ファイルを準備

---
+++ gulp/src/components/App.react.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var React = require("react");
+var AppActions = require("../AppActions");
+var AppStore = require("../AppStore");
+var MenuSection = require("./SelectSection.react");
+var InputSection = require("./InputSection.react");
+var OutputSection = require("./OutputSection.react");
